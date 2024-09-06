56ee0f742a3b1e57a87c19f1a58d3029ba4680c0
Brian Do <thebriando@gmail.com>
2024-09-05 22:56:51 -0700

    Update routes, seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
 Client.create!([
-  { name: "Client One" },
-  { name: "Client Two" },
-  { name: "Client Three" },
-  { name: "Client Four" },
-  { name: "Client Five" }
+  { name: 'Client One' },
+  { name: 'Client Two' },
+  { name: 'Client Three' },
+  { name: 'Client Four' },
+  { name: 'Client Five' }
 ])
 
-# Add custom fields and buildings...
+# Add custom fields and buildings for the clients
+Client.all.each do |client|
+  client.custom_fields.create!(name: 'Custom Field 1', field_type: 'number')
+  client.custom_fields.create!(name: 'Custom Field 2', field_type: 'freeform')
+
+  client.buildings.create!(address: '123 Main St', state: 'NY', zip: '10001')
+  client.buildings.create!(address: '456 Second St', state: 'CA', zip: '90210')
+end
