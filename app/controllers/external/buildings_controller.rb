1136940ecc36eabf57d0e7769c7cec4c3bff8d21
Brian Do <thebriando@gmail.com>
2024-09-05 23:18:12 -0700

    Add pagination

--- app/controllers/external/buildings_controller.rb
+++ app/controllers/external/buildings_controller.rb
@@ -1,10 +1,23 @@
 module External
   class BuildingsController < ApplicationController
     def index
-      buildings = Building.all
+      # Default to page 1 if no page parameter is provided
+      page = params[:page] || 1
+      per_page = params[:per_page] || 10  # Default to 10 items per page if no per_page param is provided
+
+      # Fetch paginated buildings using Kaminari
+      buildings = Building.page(page).per(per_page)
+
       render json: {
         status: "success",
-        buildings: buildings.as_json(include: { client: { only: :name }, building_custom_field_values: { only: [ :custom_field_id, :value ] } })
+        buildings: buildings.as_json(include: { client: { only: :name }, building_custom_field_values: { only: [ :custom_field_id, :value ] } }),
+        pagination: {
+          current_page: buildings.current_page,
+          next_page: buildings.next_page,
+          prev_page: buildings.prev_page,
+          total_pages: buildings.total_pages,
+          total_count: buildings.total_count
+        }
       }
     end
   end
