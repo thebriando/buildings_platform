7728154adc0e9d4ab4c4bce1ce2df069aa12ad95
Brian Do <thebriando@gmail.com>
2024-09-06 12:27:05 -0700

    Update seed to seed with 5 clients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,76 +1,44 @@
 # Clear existing data
-# BuildingCustomFieldValue.delete_all
-# Building.delete_all
-# CustomField.delete_all
-# Client.delete_all
+BuildingCustomFieldValue.delete_all
+Building.delete_all
+CustomField.delete_all
+Client.delete_all
 
-# Create Clients
-client1 = Client.create!(name: "Client One")
-client2 = Client.create!(name: "Client Two")
-client3 = Client.create!(name: "Client Three")
-
-# Define custom fields for Client One
-client1_custom_fields = [
-  { name: "bathroom_count", field_type: "number" },
-  { name: "living_room_color", field_type: "freeform" },
-  { name: "walkway_type", field_type: "enum", enum_choices: [ "Brick", "Concrete", "None" ] }
+# Create 5 clients
+clients = [
+  { name: "Client One" },
+  { name: "Client Two" },
+  { name: "Client Three" },
+  { name: "Client Four" },
+  { name: "Client Five" }
 ]
 
-client1_custom_fields.each do |field|
-  client1.custom_fields.create!(field)
+clients.each do |client_data|
+  client = Client.create!(name: client_data[:name])
+
+  # Create custom fields for each client
+  custom_fields = [
+    { name: "bathroom_count", field_type: "number" },
+    { name: "living_room_color", field_type: "freeform" },
+    { name: "walkway_type", field_type: "enum", enum_choices: [ "Brick", "Concrete", "None" ] }
+  ]
+
+  custom_fields.each do |field|
+    client.custom_fields.create!(field)
+  end
+
+  # Create buildings for each client
+  building1 = client.buildings.create!(address: "123 Main St", state: "NY", zip: "10001")
+  building2 = client.buildings.create!(address: "456 Elm St", state: "CA", zip: "90210")
+
+  # Add custom field values for each building
+  building1.building_custom_field_values.create!(custom_field: client.custom_fields.find_by(name: "bathroom_count"), value: "2.5")
+  building1.building_custom_field_values.create!(custom_field: client.custom_fields.find_by(name: "living_room_color"), value: "Blue")
+  building1.building_custom_field_values.create!(custom_field: client.custom_fields.find_by(name: "walkway_type"), value: "Brick")
+
+  building2.building_custom_field_values.create!(custom_field: client.custom_fields.find_by(name: "bathroom_count"), value: "3")
+  building2.building_custom_field_values.create!(custom_field: client.custom_fields.find_by(name: "living_room_color"), value: "Green")
+  building2.building_custom_field_values.create!(custom_field: client.custom_fields.find_by(name: "walkway_type"), value: "Concrete")
 end
 
-# Define custom fields for Client Two
-client2_custom_fields = [
-  { name: "garage_size", field_type: "number" },
-  { name: "kitchen_color", field_type: "freeform" }
-]
-
-client2_custom_fields.each do |field|
-  client2.custom_fields.create!(field)
-end
-
-# Define custom fields for Client Three
-client3_custom_fields = [
-  { name: "pool_size", field_type: "number" },
-  { name: "roof_type", field_type: "enum", enum_choices: [ "Tile", "Metal", "Shingle" ] }
-]
-
-client3_custom_fields.each do |field|
-  client3.custom_fields.create!(field)
-end
-
-# Create buildings for Client One
-building1 = client1.buildings.create!(address: "123 Main St", state: "NY", zip: "10001")
-building2 = client1.buildings.create!(address: "456 Elm St", state: "CA", zip: "90210")
-
-# Add custom field values for buildings of Client One
-building1.building_custom_field_values.create!(custom_field: client1.custom_fields.find_by(name: "bathroom_count"), value: "2.5")
-building1.building_custom_field_values.create!(custom_field: client1.custom_fields.find_by(name: "living_room_color"), value: "Blue")
-building1.building_custom_field_values.create!(custom_field: client1.custom_fields.find_by(name: "walkway_type"), value: "Brick")
-
-building2.building_custom_field_values.create!(custom_field: client1.custom_fields.find_by(name: "bathroom_count"), value: "3")
-building2.building_custom_field_values.create!(custom_field: client1.custom_fields.find_by(name: "living_room_color"), value: "Green")
-building2.building_custom_field_values.create!(custom_field: client1.custom_fields.find_by(name: "walkway_type"), value: "Concrete")
-
-# Create buildings for Client Two
-building3 = client2.buildings.create!(address: "789 Pine St", state: "TX", zip: "73301")
-building4 = client2.buildings.create!(address: "101 Oak St", state: "FL", zip: "33101")
-
-# Add custom field values for buildings of Client Two
-building3.building_custom_field_values.create!(custom_field: client2.custom_fields.find_by(name: "garage_size"), value: "2")
-building3.building_custom_field_values.create!(custom_field: client2.custom_fields.find_by(name: "kitchen_color"), value: "Red")
-
-building4.building_custom_field_values.create!(custom_field: client2.custom_fields.find_by(name: "garage_size"), value: "1")
-building4.building_custom_field_values.create!(custom_field: client2.custom_fields.find_by(name: "kitchen_color"), value: "White")
-
-# Create buildings for Client Three
-building5 = client3.buildings.create!(address: "202 Birch St", state: "AZ", zip: "85001")
-building6 = client3.buildings.create!(address: "303 Maple St", state: "NV", zip: "89101")
-
-# Add custom field values for buildings of Client Three
-building5.building_custom_field_values.create!(custom_field: client3.custom_fields.find_by(name: "pool_size"), value: "25")
-building5.building_custom_field_values.create!(custom_field: client3.custom_fields.find_by(name: "roof_type"), value: "Tile")
-
-building6.building_custom_field_values.create!(custom_field: client3.custom_fields.find_by(name: "pool_size"), value: "30")
-building6.building_custom_field_values.create!(custom_field: client3.custom_fields.find_by(name: "roof_type"), value: "Shingle")
+puts "Seeded 5 clients with buildings and custom fields."
